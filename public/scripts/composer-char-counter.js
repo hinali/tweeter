75db8a12c2b40697443acc09fcd53cdbb4a70c1b
hinali <hinali030@gmail.com>
2023-09-14 04:25:40 -0600

    implemented character counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+  const textarea = $('.new-tweet textarea');
+  const counter = $('.counter')
 
+  textarea.on('input', function() {
+    const charactersLeft = 140 - (this.value.length);
+    counter.text(charactersLeft);
+    console.log("char: ", charactersLeft);
 
+    if (charactersLeft < 0) {
+      counter.css('color', 'red');
+    } else {
+      counter.css('color', ''); 
+    }
+  });
   console.log("DOM is ready..");
 });
