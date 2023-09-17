15280aa1892b4b10f94f0d110311dc3649ecc7ba
hinali <hinali030@gmail.com>
2023-09-17 16:34:37 -0600

    When the user submits a new tweet,it shows up without refresh

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
           <label for="username" style="color:rgb(173, 181, 237)">${tweet.user.handle}</label>
         </div>
         <p class="tweet-content">${tweet.content.text}</p>
-        <footer>
+        <footer class="footer-line">
         <label>${timeago.format(tweet.created_at)}</label>
           <div class="tweet-actions footer_icons">
             <i class="fa-solid fa-flag"></i>
@@ -23,51 +23,55 @@ $(document).ready(function() {
   };
 
   const renderTweets = function(tweets) {
-
+    const $container = $('#tweets-container');
+    $container.empty(); 
+  
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $('#tweets-container').append($tweet);
+      $container.prepend($tweet);
     }
   };
-
+  
   function validateTweetText(tweetText) {
     
     if (!tweetText) {
       alert('Tweet content cannot be empty.');
-      return false; // Validation failed
+      return false;
     }
 
     if (tweetText.length > 140) {
       alert('Tweet content is too long. Maximum 140 characters allowed.');
-      return false; // Validation failed
+      return false; 
     }
 
-    return true; // Validation passed
+    return true; 
   };
 
-$('#tweet-form').submit(function(event) {
+  $('#tweet-form').submit(function(event) {
     event.preventDefault();
     const tweetText = $('#tweet-text').val();
-
+  
     if (!validateTweetText(tweetText)) {
       return;
     }
-
+  
     const formData = $(this).serialize();
-
+  
     $.ajax({
       url: '/tweets',
       method: 'POST',
       data: formData,
       success: function(response) {
         loadTweets();
+        const newTweet = createTweetElement(response);
+        $('#tweets-container').prepend(newTweet);
       },
       error: function(error) {
         console.error('Error:', error);
       }
     });
   });
-
+  
   const loadTweets = function() {
     $.ajax({
       url: '/tweets',
